6ff32675d73ffe3b680b799bc903a014b7fbaf5e
FishShalami <fishshalami@gmail.com>
2025-01-22 13:18:04 -0800

    Created functiong carousel with L/R arrows for image advance

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import "./global.css";
 import addMenuDropdown from "./menu";
 import "./carouselStyle.css";
+import { plusSlides, currentSlide, showSlides } from "./carousel.js";
 
 const menuSelectorHead = document.querySelector("header p"); //adjust selector for name of menu element/header
 const menuItemsHead = document.querySelector("header .menu");
-const menuSelectorFoot = document.querySelector("footer p"); //adjust selector for name of menu element/header
-const menuItemsFoot = document.querySelector("footer .menu");
+// const menuSelectorFoot = document.querySelector("footer p"); //adjust selector for name of menu element/header
+// const menuItemsFoot = document.querySelector("footer .menu");
 
 addMenuDropdown(menuSelectorHead, menuItemsHead);
-addMenuDropdown(menuSelectorFoot, menuItemsFoot);
+// addMenuDropdown(menuSelectorFoot, menuItemsFoot);
+
+const prevArrow = document.querySelector(".prev");
+prevArrow.addEventListener("click", () => {
+  console.log("previous arrow clicked");
+  plusSlides(-1);
+});
+
+const nextArrow = document.querySelector(".next");
+nextArrow.addEventListener("click", () => {
+  console.log("next arrow clicked");
+  plusSlides(1);
+});
