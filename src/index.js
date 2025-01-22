891a564bf00b411c40f0098e00f639a80ce58c8c
FishShalami <fishshalami@gmail.com>
2025-01-22 13:59:28 -0800

    Carousel dots functioning properly

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./global.css";
 import addMenuDropdown from "./menu";
 import "./carouselStyle.css";
 import carouselArrows from "./carouselDOM";
+import { forEach } from "lodash";
+import { currentSlide } from "./carousel";
 
 const menuSelectorHead = document.querySelector("header p"); //adjust selector for name of menu element/header
 const menuItemsHead = document.querySelector("header .menu");
@@ -12,3 +14,13 @@ addMenuDropdown(menuSelectorHead, menuItemsHead);
 // addMenuDropdown(menuSelectorFoot, menuItemsFoot);
 
 carouselArrows();
+
+const dotSelector = document.querySelectorAll(".dot");
+
+dotSelector.forEach((dot) => {
+  dot.addEventListener("click", (event) => {
+    const dotIndex = dot.getAttribute("data-index");
+    console.log(`Dot ${dotIndex} was clicked`);
+    currentSlide(dotIndex);
+  });
+});
